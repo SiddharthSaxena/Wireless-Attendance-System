58ee223be737774b03f5aaa6bad85b0cfef60f79
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-10-06 17:51:37 +0530

    Update QR Encoding

--- src/QR Encoding.py
+++ src/QR Encoding.py
@@ -2,7 +2,7 @@ import pyqrcode
 
 encode = pyqrcode.create('Wireless Attendance System', error='Q', version=4, mode='binary')
 
-# Generate PNG image of QR code.
+# Generate PNG image of QR codes.
 
 encode.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
 encode.show()
