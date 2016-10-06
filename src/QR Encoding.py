23c92636bc8c9e277c1d0598c4cab5fc383cf481
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-10-06 17:16:25 +0530

    Added License

---
+++ src/QR Encoding.py
@@ -0,0 +1,8 @@
+import pyqrcode
+
+encode = pyqrcode.create('Wireless Attendance System', error='Q', version=4, mode='binary')
+
+# Generate PNG image of QR code.
+
+encode.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+encode.show()
