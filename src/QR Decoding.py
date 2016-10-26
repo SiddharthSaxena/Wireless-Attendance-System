9b8f3ce7bbd0be21c10a6b73e4b317f6eb68034f
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-10-26 18:41:41 +0530

    Update QR Decoding

--- src/QR Decoding.py
+++ src/QR Decoding.py
@@ -6,8 +6,9 @@ import numpy
 
 cap = cv2.VideoCapture(0)
 
+
 def qrCheck(arg):
-    """ Check an image for a QR code, return as string """
+    # Check an image for a QR code, return as string.
     # file_path = '/home/siddharth/Desktop/code.png'
     # with open(file_path, 'rb') as image_file:
     #     image = Image.open(image_file)
@@ -16,24 +17,24 @@ def qrCheck(arg):
     try:
         code = zbarlight.scan_codes('qrcode', image_string)
         return code
-    except:
+    except AssertionError:
         return
 
-while(True):
-    # Capture frame-by-frame
+while True:
+    # Capture frame-by-frame.
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    # Our operations on the frame come here.
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     decoded = qrCheck(gray)
     print(decoded)
 
-# Display the resulting frame
+# Display the resulting frame.
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+# When everything done, release the capture.
 cap.release()
 cv2.destroyAllWindows()
