411a1bf9f20bc6b3e7b0b4dc2f90256e177f1aa4
SiddharthSaxena <siddharth.saxena25@gmail.com>
2016-11-24 14:53:42 +0530

    Added support for Python 2 and 3

--- src/QR Encoding.py
+++ src/QR Encoding.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python3
 import pyqrcode
+import sys
 
-code = input('Enter the data to be converted into QR code: ')
-encode = pyqrcode.create(code, error='Q', version=4, mode='binary')
 
-# Generate PNG image of QR codes.
-encode.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+def version():
+    major = int(sys.version.strip().split('.')[0])
+    return major
+
+
+def encode(data):
+    encoded = pyqrcode.create(data, error='Q', version=4, mode='binary')
+    encoded.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+    return encoded
+
+if version() == 2:
+    code = raw_input('Enter the data to be converted into QR code: ')
+    encode(code)
+elif version() == 3:
+    code = input('Enter the data to be converted into QR code: ')
+    encode(code)
+else:
+    print('Unknown python version {}'.format(version()))
